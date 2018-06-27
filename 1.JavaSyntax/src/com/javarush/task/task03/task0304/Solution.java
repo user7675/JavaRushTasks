d27ce14c8245505a2d4307b4c773f6f9dd20c2fd
user7675 <arkashka_@mail.ru>
2018-06-27 16:16:27 +0300

    0304 a

--- 1.JavaSyntax/src/com/javarush/task/task03/task0304/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0304/Solution.java
@@ -6,7 +6,7 @@ package com.javarush.task.task03.task0304;
 
 public class Solution {
     public static double addTenPercent(int i) {
-        //напишите тут ваш код
+        return i*1.1;
     }
 
     public static void main(String[] args) {
