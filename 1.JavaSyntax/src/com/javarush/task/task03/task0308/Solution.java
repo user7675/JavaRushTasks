9dbecf6ba666186ccd737378dace8f750f89c439
user7675 <arkashka_mail.ru>
2018-06-29 21:42:52 +0300

    0308 a

--- 1.JavaSyntax/src/com/javarush/task/task03/task0308/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0308/Solution.java
@@ -6,6 +6,11 @@ package com.javarush.task.task03.task0308;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        int result = 1;
+        for (int i = 1; i < 11; i++){
+            result *= i;
+
+        }
+        System.out.println(result);
     }
 }
